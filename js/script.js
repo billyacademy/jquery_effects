e1f83c1fbca09b0b234be4f278e6737cc1e35796
William Mahoney <wm.j.mahoney@gmail.com>
2015-02-09 13:16:05 -0500

    removed active from html, will make use of it in js, alert working on clicking next

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-  alert("Document is loaded");
+  //Set Options
+  var speed = 500;              // fade speed
+  var autoSwitch = true;        //auto slider options
+  var autoswitch_speed = 4000   // Auto slider speed
+
+
+  // Add initial active class
+  $('.slide').first().addClass('active');
+
+  // Hide all slides
+  $('.slide').hide();
+
+  // Show first slide
+  $('.active').show();
+
+  // event handler
+
+  $('#next').on('click', function() {
+    alert('Next was clicked');
+  });
 });
