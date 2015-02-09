27d92ba1e130e0a71a4df0d9b066acd5856cd122
William Mahoney <wm.j.mahoney@gmail.com>
2015-02-09 13:25:15 -0500

    slider is working

--- js/script.js
+++ js/script.js
@@ -17,6 +17,29 @@ $(document).ready(function(){
   // event handler
 
   $('#next').on('click', function() {
-    alert('Next was clicked');
+    $('.active').removeClass('active').addClass('oldActive');
+    if($('.oldActive').is(':last-child')){
+      // last child is asking if it was the last slide
+      $('.slide').first().addClass('active');
+    } else {
+      $('.oldActive').next().addClass('active');
+    }
+    $('.oldActive').removeClass('oldActive');
+    $('.slide').fadeOut(speed);
+    $('.active').fadeIn(speed);
   });
+
+  $('#prev').on('click', function() {
+    $('.active').removeClass('active').addClass('oldActive');
+    if($('.oldActive').is(':first-child')){
+      // last child is asking if it was the last slide
+      $('.slide').last().addClass('active');
+    } else {
+      $('.oldActive').prev().addClass('active');
+    }
+    $('.oldActive').removeClass('oldActive');
+    $('.slide').fadeOut(speed);
+    $('.active').fadeIn(speed);
+  });
+
 });
